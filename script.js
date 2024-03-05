03b4785048d27ccd5ae84759ee92ee9e9ce11ea6
channeloronico <155803043+channeloronico@users.noreply.github.com>
2024-03-06 00:52:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-
 var button = document.getElementById("switch-button");
-
 var mode = "light";
+var body = document.body;
 
 button.addEventListener("click", function() {
-  var body = document.body;
-
   if (mode === "light") {
     // Mode to Dark
     mode = "dark";
-
     // Dark Mode GIF
-    body.style.backgroundImage = "url('https://media1.giphy.com/media/qTeiUJNlcoMe3LOMkk/giphy.gif?cid=ecf05e475cfhbotf1cihgxif1mslerua9wb4biu4j4pkyhbm&ep=v1_gifs_related&rid=giphy.gif&ct=g')";
-
+    body.style.backgroundImage = "var(--dark-mode-background)";
   } else {
     // Mode to Light
     mode = "light";
-
     // Light Mode GIF
-    body.style.backgroundImage = "url('https://media0.giphy.com/media/ddfb7PvwyzrepUhsdZ/giphy.gif?cid=ecf05e475cfhbotf1cihgxif1mslerua9wb4biu4j4pkyhbm&ep=v1_gifs_related&rid=giphy.gif&ct=g')";
-
+    body.style.backgroundImage = "var(--light-mode-background)";
   }
 });
